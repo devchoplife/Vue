aa569f258a5783e938cb67e264673b1f813acf19
ChopLife <dritedurojaiye@gmail.com>
2022-05-06 12:14:58 +0100

    Add code for Random User Generator

--- Random User Generator/app.js
+++ Random User Generator/app.js
@@ -1,5 +1,27 @@
 const app = Vue.createApp({
-    template: '<h1>Hello World</h1>',
+    data() {
+        return {
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'john@gmail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg'
+        }
+    },
+    methods: {
+        async getUser() {
+            const res = await fetch('https://randomuser.me/api')
+            const { results } = await res.json()
+
+            console.log(results)
+
+            this.firstName = 'Joy',
+                this.lastName = 'Smith',
+                this.email = 'joy@gmail.com',
+                this.gender = 'female',
+                this.picture = 'https://randomuser.me/api/portraits/women/10.jpg'
+        },
+    }
 })
 
 app.mount('#app')
